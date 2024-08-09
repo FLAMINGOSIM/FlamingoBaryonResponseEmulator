a9bf0aa4a4f759cf43108fe4c283c7de3937229e
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2024-08-09 16:50:15 +0200

    Update the C-OWLS MCMC to try fitting all 4 models

--- mcmc_cowls.py
+++ mcmc_cowls.py
@@ -52,52 +52,88 @@ rc("font", **{"family": "sans-serif", "sans-serif": ["Times"]})
 
 # Load FLAMINGO emulator
 from flamingo_response_emulator import FlamingoBaryonResponseEmulator
+
 emulator = FlamingoBaryonResponseEmulator()
 
 ############################################
 
-data_cowls = np.loadtxt("./VD20/powtable_C-OWLS_AGN_WMAP7.dat")
-cowls_z = data_cowls[:,0]
-cowls_k = data_cowls[:,1]
-cowls_P = data_cowls[:,2]
-data_DMO = np.loadtxt("./VD20/powtable_DMONLY_WMAP7_L400N1024.dat")
-cowls_DMO_P = data_DMO[:,2]
-cowls_R = cowls_P / cowls_DMO_P
+all_cowls_short_names = ["cowl_agn", "cowl_agn8p3", "cowl_agn8p5", "cowl_agn8p7"]
+all_cowls_labels = [
+    "C-OWLS-AGN",
+    "C-OWLS-AGN-$\\Delta$T8.3",
+    "C-OWLS-AGN-$\\Delta$T8.5",
+    "C-OWLS-AGN-$\\Delta$T8.7",
+]
+all_cowls_fname = [
+    "powtable_C-OWLS_AGN_WMAP7.dat",
+    "powtable_C-OWLS_AGN_Theat8.3_WMAP7.dat",
+    "powtable_C-OWLS_AGN_Theat8.5_WMAP7.dat",
+    "powtable_C-OWLS_AGN_Theat8.7_WMAP7.dat",
+]
+all_cowls_colors = cm.plasma(np.linspace(0.0, 0.9, len(all_cowls_labels)))
+all_cowls_fit = []
+all_cowls_k = []
+all_cowls_R = []
 
-mask = (cowls_z == 0.)
-cowls_k = cowls_k[mask]
-cowls_R = cowls_R[mask]
-cowls_z = cowls_z[mask]
+for i in range(len(all_cowls_fname)):
 
-# Correct spurious large-scale excess power
-cowls_R[cowls_R > 1.] = 1.
+    data_cowls = np.loadtxt("./VD20/%s" % all_cowls_fname[i])
+    cowls_z = data_cowls[:, 0]
+    cowls_k = data_cowls[:, 1]
+    cowls_P = data_cowls[:, 2]
 
-# Select the range overlapping with the emulator
-mask = np.logical_and(cowls_k > k_min, cowls_k < k_max)
-cowls_k = cowls_k[mask]
-cowls_R = cowls_R[mask]
+    data_DMO = np.loadtxt("./VD20/powtable_DMONLY_WMAP7_L400N1024.dat")
+    cowls_DMO_P = data_DMO[:, 2]
+    cowls_DMO_z = data_DMO[:, 0]
+
+    # Select z = 0 only
+    mask = cowls_DMO_z == 0.0
+    cowls_DMO_P = cowls_DMO_P[mask]
+    cowls_DMO_z = cowls_DMO_z[mask]
+
+    mask = cowls_z == 0.0
+    cowls_k = cowls_k[mask]
+    cowls_z = cowls_z[mask]
+    cowls_P = cowls_P[mask]
+
+    cowls_R = cowls_P / cowls_DMO_P
+
+    # Correct spurious large-scale excess power
+    cowls_R[cowls_R > 1.0] = 1.0
+
+    # Select the range overlapping with the emulator
+    mask = np.logical_and(cowls_k > k_min, cowls_k < k_max)
+    all_cowls_k.append(cowls_k[mask])
+    all_cowls_R.append(cowls_R[mask])
+
 
 # Create smooth function
-def ratio_cowls(k):
-    interpolator = inter.CubicSpline(np.log10(cowls_k), cowls_R)
+def ratio_cowls(k, i):
+    interpolator = inter.CubicSpline(np.log10(all_cowls_k[i]), all_cowls_R[i])
     return interpolator(np.log10(k))
 
-############################################
 
+# ############################################
+
+# Create some generic MCMC properties
 import emcee
+import corner
 from multiprocessing import Pool
+
 
 def model(theta, log10_k):
     sigma_gas, sigma_star, jet = theta
     return emulator.predict(10**log10_k, 0.0, sigma_gas, sigma_star, jet, False)
 
+
 def lnlike(theta, x, y, yerr):
     return -0.5 * np.sum(((y - model(theta, x)) / yerr) ** 2)
+
 
 def lnprior(theta):
     sigma_gas, sigma_star, jet = theta
     if (
-        sigma_gas > -8.0
+        sigma_gas > -9.0
         and sigma_gas < 4.0
         and sigma_star > -2.0
         and sigma_star < 1.0
@@ -116,42 +152,39 @@ def lnprob(theta, x, y, yerr):
     return lp + lnlike(theta, x, y, yerr)
 
 
-mask = np.logical_and(bins_k_data >= 0.099, bins_k_data < 20)
-bins_k_mcmc = bins_k_data[mask]
-data = (np.log10(bins_k_mcmc), ratio_cowls(bins_k_mcmc), 0.0001 * bins_k_mcmc)
-nwalkers = 256
-niter = 4000
-initial = np.array([2.0, 0.5, 0.7])
-ndim = len(initial)
-p0 = [np.array(initial) + 1e-7 * np.random.randn(ndim) for i in range(nwalkers)]
-
-
 def main(p0, nwalkers, niter, ndim, lnprob, data):
 
     with Pool() as pool:
         sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=data, pool=pool)
-
         print("Running burn-in...")
         p0, _, _ = sampler.run_mcmc(p0, 100, progress=True)
         sampler.reset()
-
         print("Running production...")
         pos, prob, state = sampler.run_mcmc(p0, niter, progress=True)
-
         return sampler, pos, prob, state
 
 
-sampler, pos, prob, state = main(p0, nwalkers, niter, ndim, lnprob, data)
+# Run MCMC for each model
+for i in range(len(all_cowls_k)):
+    mask = np.logical_and(bins_k_data >= 0.049, bins_k_data < 20)
+    bins_k_mcmc = bins_k_data[mask]
+    data = (np.log10(bins_k_mcmc), ratio_cowls(bins_k_mcmc, i), 0.0001 * bins_k_mcmc)
+    nwalkers = 256
+    niter = 4000
+    initial = np.array([2.0, 0.5, 0.7])
+    ndim = len(initial)
+    p0 = [np.array(initial) + 1e-7 * np.random.randn(ndim) for i in range(nwalkers)]
 
-samples = sampler.flatchain
-theta_max = samples[np.argmax(sampler.flatlnprobability)]
-best_fit_model = model(theta_max, np.log10(bins_k_data))
-cowls_model_fit = theta_max
+    sampler, pos, prob, state = main(p0, nwalkers, niter, ndim, lnprob, data)
 
-import corner
-flat_samples = sampler.get_chain(discard=100, thin=15, flat=True)
-fig = corner.corner(flat_samples, labels=["fgas", "M*", "jet"])
-fig.savefig("COWLS_fitting_params.png")
+    samples = sampler.flatchain
+    theta_max = samples[np.argmax(sampler.flatlnprobability)]
+    best_fit_model = model(theta_max, np.log10(bins_k_data))
+    all_cowls_fit.append(theta_max)
+
+    flat_samples = sampler.get_chain(discard=100, thin=15, flat=True)
+    fig = corner.corner(flat_samples, labels=["fgas", "M*", "jet"])
+    fig.savefig("COWLS_fitting_params_%s.png" % all_cowls_short_names[i])
 
 ############################################
 
@@ -181,30 +214,37 @@ ax.plot(
     bins_k,
     pred_R,
     ls="-",
-    color=colors_z[0],
+    color="k",
     lw=1,
-    label="${\\rm FLAMINGO~fiducial}~{\\rm(emulator)} $",
+    label="${\\rm FLAMINGO~fid.}~{\\rm(emul.)} $",
 )
 
 # Plot other sim data
-#ax.plot(cowls_k, cowls_R, "-", color="0.5", lw=0.7, label="C-OWLS-AGN")
-ax.plot(bins_k, ratio_cowls(bins_k), '-', color='0.5', lw=0.7, label="C-OWLS-AGN")
-
-print(cowls_model_fit)
+for i in range(len(all_cowls_k)):
+    # ax.plot(cowls_k, cowls_R, "-", color="0.5", lw=0.7, label="C-OWLS-AGN")
+    ax.plot(
+        bins_k,
+        ratio_cowls(bins_k, i),
+        "--",
+        color=all_cowls_colors[i],
+        lw=0.7,
+        label=all_cowls_labels[i],
+    )
 
 # Plot COWLS emulator best-fit
-pred_R, pred_var_R = emulator.predict(
-    bins_k, redshift, cowls_model_fit[0], cowls_model_fit[1], cowls_model_fit[2]
-)
-ax.plot(
-    bins_k,
-    pred_R,
-    ls="-",
-    color="g",
-    lw=1,
-    label="emulator fit: $%d\\%%{\\rm JET~fgas}%+3.2f\\sigma~{\\rm M*}%+3.2f\\sigma$"
-    % (cowls_model_fit[2] * 100, cowls_model_fit[0], cowls_model_fit[1]),
-)
+for i in range(len(all_cowls_k)):
+    pred_R, pred_var_R = emulator.predict(
+        bins_k, redshift, all_cowls_fit[i][0], all_cowls_fit[i][1], all_cowls_fit[i][2]
+    )
+    ax.plot(
+        bins_k,
+        pred_R,
+        ls="-",
+        color=all_cowls_colors[i],
+        lw=1,
+        label="emul. fit: $%d\\%%{\\rm JET~fgas}%+3.2f\\sigma~{\\rm M*}%+3.2f\\sigma$"
+        % (all_cowls_fit[i][2] * 100, all_cowls_fit[i][0], all_cowls_fit[i][1]),
+    )
 
 
 # Plot range
@@ -219,20 +259,21 @@ ax.vlines(k_max, -100, 100, "k", ls=":", lw=0.7)
 
 # Legend and model
 legend = ax.legend(
-    loc="lower left",
+    loc="upper left",
     fancybox=True,
     framealpha=1,
     handlelength=1,
-    ncol=1,
+    ncol=2,
     columnspacing=0.8,
     handletextpad=0.5,
+    fontsize=7,
 )
 legend.get_frame().set_edgecolor("white")
 ax.text(
     k_min * 1.2,
-    1.13,
+    0.73,
     "$z=%3.2f$" % redshift,
-    va="top",
+    va="bottom",
     ha="left",
 )
 
