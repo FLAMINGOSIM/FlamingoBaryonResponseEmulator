8b0dfdaa0fdf5fdb5edc33a40bc7d7c568d7ac9f
Matthieu Schaller <schaller@strw.leidenuniv.nl>
2024-11-11 22:07:30 +0100

    Some more documentation of the class

--- FlamingoBaryonResponseEmulator/flamingo_response_emulator.py
+++ FlamingoBaryonResponseEmulator/flamingo_response_emulator.py
@@ -3,10 +3,10 @@ from scipy import interpolate as inter
 import swiftemulator.emulators.gaussian_process as se
 import pickle
 import lzma
-import attr
+from attr import define
 
 
-# @attr.s
+# @define
 class FlamingoBaryonResponseEmulator:
     """
     Emulator for the baryon response of the matter power spectrum in
@@ -16,10 +16,10 @@ class FlamingoBaryonResponseEmulator:
 
     min_k: float = -1.5
     max_k: float = 1.5
-    delta_bins_k: float
-    k_bins: np.array
+    delta_bins_k: float = None
+    k_bins: np.array = None
     num_bins_k: int = 31
-    PS_ratio_emulator: se.GaussianProcessEmulator
+    PS_ratio_emulator: se.GaussianProcessEmulator = None
 
     def load_emulator(self):
         """
@@ -32,8 +32,48 @@ class FlamingoBaryonResponseEmulator:
             self.PS_ratio_emulator = pickle.load(f)
 
     def predict(
-        self, k_: np.array, z: float, sigma_gas: float, sigma_star: float, jet: float
-    ):
+        self, k: np.array, z: float, sigma_gas: float, sigma_star: float, jet: float
+    ) -> np.array:
+        """
+        Returns the predicted baryonic response for a set of comoving modes,
+        redshift, and galaxy formation model (three parameters).
+
+        Parameters
+        ----------
+
+        k: np.array
+            The Fourier modes at which the baryonic response has to be evaluated
+            expressed in units of [h / Mpc].
+
+        z: float
+            The redshift at which the baryonic response has to be evaluated.
+            The value has to be between 0 and 2.
+
+        sigma_gas: float
+
+        sigma_far: float
+
+        jet: float
+
+        Returns
+        -------
+
+        baryon_ratio: np.array
+            The baryonic response at the modes k specified in the input.
+
+        Raises
+        ------
+
+        ValueError
+            When the input redshift is not in the range [0, 2].
+
+        """
+
+        # Verify the validity of the redshift
+        if z < 0.0 or z > 2.0:
+            raise ValueError(
+                "The emulator has only been trained for redshifts between 0 and 2."
+            )
 
         # Construct parameters in emulator space.
         predictparams = {
@@ -52,16 +92,22 @@ class FlamingoBaryonResponseEmulator:
         ratio_interpolator = inter.CubicSpline(self.k_bins, ratio)
 
         # Return the interpolated ratios
-        ret_ratio = ratio_interpolator(np.log10(k_))
+        baryon_ratio = ratio_interpolator(np.log10(k))
 
         # Set the ratio at k-values below min_k to 1
-        ret_ratio[k_ < 10**self.min_k] = ratio_interpolator(self.min_k)
+        baryon_ratio[k < 10**self.min_k] = ratio_interpolator(self.min_k)
 
-        return ret_ratio
+        return baryon_ratio
 
     def predict_with_variance(
-        self, k_: np.array, z: float, sigma_gas: float, sigma_star: float, jet: float
+        self, k: np.array, z: float, sigma_gas: float, sigma_star: float, jet: float
     ):
+
+        # Verify the validity of the redshift
+        if z < 0.0 or z > 2.0:
+            raise ValueError(
+                "The emulator has only been trained for redshifts between 0 and 2."
+            )
 
         # Construct parameters in emulator space.
         predictparams = {
@@ -81,12 +127,12 @@ class FlamingoBaryonResponseEmulator:
         variance_interpolator = inter.CubicSpline(self.k_bins, variance)
 
         # Return the interpolated ratios
-        ret_ratio = ratio_interpolator(np.log10(k_))
-        ret_variance = variance_interpolator(np.log10(k_))
+        ret_ratio = ratio_interpolator(np.log10(k))
+        ret_variance = variance_interpolator(np.log10(k))
 
         # Set the ratio at k-values below min_k to 1
-        ret_ratio[k_ < 10**self.min_k] = ratio_interpolator(self.min_k)
-        ret_variance[k_ < 10**self.min_k] = 0.0
+        ret_ratio[k < 10**self.min_k] = ratio_interpolator(self.min_k)
+        ret_variance[k < 10**self.min_k] = 0.0
 
         return ret_ratio, ret_variance
 
